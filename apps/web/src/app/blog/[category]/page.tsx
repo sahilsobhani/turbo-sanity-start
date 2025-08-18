7e1fb7d35d72b0ac5312555dd4b90e27a5bae59f
Sahil Sobhani <sahilsobhani4@gmail.com>
2025-08-18 18:31:43 +0530

    UI/UX: added a link icon for every hit for better accessibility

--- apps/web/src/app/blog/[category]/page.tsx
+++ apps/web/src/app/blog/[category]/page.tsx
@@ -6,6 +6,7 @@ import { queryBlogsByCategory } from "@/lib/sanity/query";
 import { getSEOMetadata } from "@/lib/seo";
 import { handleErrors } from "@/utils";
 import CategoryList from "@/components/sections/category-list";
+import Link from "next/link";
 
 //what: added search section
 //why: to display the search box and exhibit search results
@@ -30,7 +31,7 @@ export async function generateMetadata(props: BlogCategoryPageProps) {
   const params = await props.params;
 
   return getSEOMetadata({
-    title: `${params.category} Blog Posts  | Turbo Start Sanity`, 
+    title: `${params.category} Blog Posts  | Turbo Start Sanity`,
     description: `Discover expert insights and tutorials about  ${params.category}. Browse our collection of in-depth articles and guides.`,
     slug: `/blog/${params.category}`,
   });
@@ -43,15 +44,17 @@ export default async function BlogCategoryPage(props: BlogCategoryPageProps) {
   if (err || !res?.data) notFound();
 
   const blogs = res.data;
- 
+
 
   if (!blogs.length) {
     return (
       <main className="container my-16 mx-auto px-4 md:px-6">
         <SearchSection />
-        <h1 className="text-4xl font-medium mb-4 uppercase">Blog / {params.category}</h1>
+        <h1 className="text-4xl font-medium mb-4 uppercase">
+          <Link className='text-gray-300' href="/blog">Blog</Link> / {params.category}
+        </h1>
         <div className="h-[1px] mb-2 w-1/6 dark:bg-gray-100/10 bg-gray-700/10 rounded-full "></div>
-        <div className="text-lg font-geist mb-4 md:pr-96 md:mr-20 overflow-hidden"> 
+        <div className="text-lg font-geist mb-4 md:pr-96 md:mr-20 overflow-hidden">
           Discover expert insights and tutorials about {params.category}. Browse our collection of in-depth articles and guides.
           We share discourse on the latest tech. Keep up-to-date with content operating systems, workflows, scalability and sometimes, the odd CMS roast
         </div>
@@ -70,9 +73,11 @@ export default async function BlogCategoryPage(props: BlogCategoryPageProps) {
     <main className="bg-background">
       <div className="container my-16 mx-auto px-4 md:px-6">
         <SearchSection />
-        <h1 className="text-4xl font-medium mb-4 uppercase">Blog / {params.category} </h1>
+        <h1 className="text-4xl font-medium mb-4 uppercase">
+          <Link className="text-gray-300" href="/blog">Blog</Link> / {params.category}
+        </h1>
         <div className="h-[1px] mb-2 w-1/6 dark:bg-gray-100/10 bg-gray-700/10 rounded-full "></div>
-        <div className="text-lg font-geist mb-4 md:pr-96 md:mr-20 text-muted-foreground overflow-hidden"> 
+        <div className="text-lg font-geist mb-4 md:pr-96 md:mr-20 text-muted-foreground overflow-hidden">
           Discover expert insights and tutorials about {params.category}. Browse our collection of in-depth articles and guides.
           We share discourse on the latest tech. Keep up-to-date with content operating systems, workflows, scalability and sometimes, the odd CMS roast
         </div>
